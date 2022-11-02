2698f0aed3302fd158f1f8a2b2a22b78c4468c18
Active <56242467+doubleactii@users.noreply.github.com>
2022-11-02 09:44:25 -0500

    fix sanity check

--- src/EUtils.js
+++ src/EUtils.js
@@ -226,7 +226,7 @@
 		}
 	}
 	const EUtils = new EUtilsManager();
-	if (typeof(VYLO.global) !== 'undefined') VYLO.global.EUtils = EUtils;
+	if (typeof(VYLO) !== 'undefined') VYLO.global.EUtils = EUtils;
 	window.EUtils = EUtils;
 	console.log("%cEUtils.js: ✅ EUtils.js@" + EUtils.version, "font-family:arial;");	
 })();
